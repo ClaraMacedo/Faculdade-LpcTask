3863f3a9ed8a378066b7997d57518164001d4c62
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-02-20 21:19:59 -0300

    Terceiro Commit

---
+++ task/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+class Tarefa(object):
+    """docstring for ."""
+    def __init__(self, titulo, data):
+        self.titulo = titulo
+        self.data = data
+        
+    def __str__(self):
+        return self.titulo
+
+def home(request):
+    return HttpResponse("Olá")
+
+def sobre(request):
+    return HttpResponse("Clara Macêdo")
+
+def tarefa(request, ano, mes, dia):
+    return HttpResponse("Tarefa: "+str(dia)+"-"+str(mes)+"-"+str(ano))
